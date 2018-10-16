46fd43eeecd48690cad7a67025b6e18b9cbe5e87
pmanrriq <manrrique.pablo@gmail.com>
2018-10-16 14:59:42 -0300

    Seccion 12 terminada - backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// ============================================
+// Google
+// ============================================
+
+module.exports.CLIENT_ID = '87994512546-n6ki2s9f2h2747j5ola484cg9eqgmeu9.apps.googleusercontent.com';
